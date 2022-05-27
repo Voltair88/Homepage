14e563f1bc7a0c07440af5a2a4f5e498c72b9e34
Mattias Gulec <Voltair88@gmail.com>
2022-05-27 21:52:35 +0200

    fix: removed unused var

--- src/components/RightNav.jsx
+++ src/components/RightNav.jsx
@@ -5,7 +5,6 @@ import Toggle from "./Toggle";
 import { lightTheme, darkTheme } from "../theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "../global";
-import { NavHashLink } from "react-router-hash-link";
 
 const RightNav = ({ open }) => {
   const [theme, toggleTheme, componentMounted] = useDarkMode();
@@ -18,27 +17,6 @@ const RightNav = ({ open }) => {
     <ThemeProvider theme={themeMode}>
       <GlobalStyles />
       <Ul open={open}>
-        {/*         <div className="div-link">
-          <NavHashLink to="/#home" className="Navlink">
-            Home
-          </NavHashLink>
-        </div>
-        <div className="div-link">
-          <NavHashLink to="/#about" className="Navlink">
-            About
-          </NavHashLink>
-        </div>
-        <div className="div-link">
-          <NavHashLink to="/#portfolio" className="Navlink">
-            Portfolio
-          </NavHashLink>
-        </div>
-        <div className="div-link">
-          <NavHashLink to="/#resume" className="Navlink">
-            Resumé
-          </NavHashLink>
-        </div>
- */}{" "}
         <Toggle theme={theme} toggleTheme={toggleTheme} />
       </Ul>
     </ThemeProvider>
